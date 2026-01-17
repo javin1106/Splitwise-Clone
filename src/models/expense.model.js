3f37b03dc287892d4afa569aed33e711b315e45f
Javin Chutani <javin.chutani@gmail.com>
2026-01-17 23:47:18 +0530

    Init: Added user, group, expense Models and utils

---
+++ src/models/expense.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const expenseSchema = new mongoose.Schema(
+  {
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+      required: true,
+    },
+
+    paidBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+
+    participants: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        share: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+
+    splitType: {
+      type: String,
+      enum: ["EQUAL"],
+      default: "EQUAL",
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Expense", expenseSchema);
---
+++ src/models/group.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const groupSchema = new mongoose.Schema(
+  {
+    groupName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Group = mongoose.model('Group', groupSchema);
+
+export default Group;
